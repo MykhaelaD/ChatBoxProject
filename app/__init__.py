fdbf49b31744475d9962ce64468af8706cfbb198
Graham Preston <grahampreston@comcast.net>
2022-11-13 22:44:14 -0500

    Using Flask-WTF

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+
+class Config(object):
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "GoMount1808!"
+
+app.config.from_object(Config)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,29 @@
 from flask import render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 from app import app
 
-@app.route("/")
-@app.route("/index")
+class QueryForm(FlaskForm):
+    query = StringField()
+    submit = SubmitField("Send")
+
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
-
-@app.route("/message", methods=["POST"])
-def message():
-    css_class: str = request.form.get("css_class")
-    text: str = request.form.get("text")
-
-    return render_template("message.html", css_class=css_class, text=text)
+    form = QueryForm()
+    if form.validate_on_submit():
+        queryElement: str = render_template("message.html", css_class="query", text=form.query.data)
+        response: str = f"Response: '{form.query.data}'"
+        responseElement: str = render_template("message.html", css_class="response", text=response)
+        print(form.query.data)
+        return queryElement + responseElement
+    return render_template("index.html", form=form)
 
 @app.route("/response", methods=["POST"])
 def response():
     query: str = request.form.get("query")
+    queryElement: str = render_template("message.html", css_class="query", text=query)
+    response: str = f"Response: '{query}'"
+    responseElement: str = render_template("message.html", css_class="response", text=response)
     print(query)
-    return f"Response: '{query}'"
+    return queryElement + responseElement
