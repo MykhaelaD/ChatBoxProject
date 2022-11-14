720a9a89a05a5ff9ed89414026ab6870b5a4bfef
Graham Preston <grahampreston@comcast.net>
2022-11-13 21:17:14 -0500

    Added message HTML to python

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,20 @@
 from flask import render_template, request
 from app import app
 
-@app.route('/')
-@app.route('/index')
-def index():       
-    return render_template('index.html')
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
 
 @app.route("/message", methods=["POST"])
 def message():
-    return render_template('message.html')
+    css_class: str = request.form.get("css_class")
+    text: str = request.form.get("text")
 
-@app.route('/response', methods=["POST"])
+    return render_template("message.html", css_class=css_class, text=text)
+
+@app.route("/response", methods=["POST"])
 def response():
-    query = request.form.get("query")
+    query: str = request.form.get("query")
     print(query)
     return f"Response: '{query}'"
